366ce59fe85602236f3149c6c997d930441813a5
Anu9969 <sharmaanurag9969@gmail.com>
2024-08-24 10:25:28 +0530

    nothing

--- My Journey/Events.sol
+++ My Journey/Events.sol
@@ -1,28 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >0.4.16 < 0.9.0;
 
-// 1. create a twitter contract
-// 2. create a mapping between user and tweet
-// 3. Add function to create a tweet and save it in mapping 
-// 4. Create a function to get tweet
 
-
-//Adding Struct
-// 1.Define a tweet struct with author , content ,timestamp, likes
-// 2. Add the struct to array
-// 3. Test tweets
-
-//Add a functiion called changeTweetlength to change max tweet length
-// create a constructor function to set an owner of the contract
-// create a modifier called onlyOwner
-// use onlyOwner on the ChangeTweetLength function
-
-
-//Add id to tweet struct to make every tweet unique
-// set the id to be the tweet[] Max_Tweet_Length
-// add a function to like the Tweet
-// add afunction to unlike the tweet 
-// mark both function external
 
 
 contract Twitter{
@@ -38,8 +17,7 @@ contract Twitter{
         uint256 likes;
     }
 
-    // mapping(address => string) public tweets;
-    //  mapping(address => string []) public tweets;//added array of strings for multiple tweets
+  
     mapping(address => Tweet[]) public tweets; //replaced string array with tweet struct
     address public owner;
  
@@ -57,9 +35,7 @@ contract Twitter{
 }
 
     function createTweet(string memory _tweet) public {
-        // tweets[msg.sender] = _tweet;
-        // tweets[msg.sender].push(_tweet);
-
+  
         //conditional
         //if tweet length <= 280 than we are good ,otherwise we revert
 
@@ -91,20 +67,12 @@ contract Twitter{
         tweets[author][id].likes--;
     }
 
-    // function getTweets(address _owner) public view returns (string memory){
-    //     return tweets[_owner];
-    // }
-    //  function getTweets(address _owner , uint _i) public view returns (string memory){
-    //     return tweets[_owner][_i];//added the index of tweet from array
-    // }
 
     function getTweets(address _owner ,uint _i) public view returns(Tweet memory){
         return tweets[_owner][_i]; // replaced string eturn to struct 
     }
 
-    // function getAllTweets(address _owner ) public view returns(string[] memory){
-    //     return tweets[_owner];
-    // }
+
 
     function getAllTweets(address _owner ) public view returns (Tweet[] memory){
         return tweets[_owner];
